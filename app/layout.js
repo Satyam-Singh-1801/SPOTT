5e4ed5492040c120779f46753d3adc6371eb8867
SATYAM SINGH <satyam6688singh@gmail.com>
2026-01-03 21:27:26 +0530

    Create database schema

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
+import Header from "@/components/header";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-
-
+import { ConvexClientProvider } from "./ConvexClientProvider";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export const metadata = {
   title: "Spott",
@@ -21,21 +23,31 @@ export default function RootLayout({ children }) {
             disableTransitionOnChange
           >
 
-          
+            <ClerkProvider 
+              appearance={{
+                theme: dark,
+                }}
+              >
+              <ConvexClientProvider>
+                {/* Header */}
+                <Header/>
 
-            {/* Header */}
+                <main className="relative min-h-screen container mx-auto pt-40 md:pt-32">
+                  {/* glow */}
+                  <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
+                    <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl"/>
+                    <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-orange-600/20 rounded-full blur-3xl"/>
+                  </div>
 
-            <main className="relative min-h-screen container mx-auto pt-40 md:pt-32">
-              {/* glow */}
-              <div className="pointer-events-none absolute inset-0 -z-10">
-                <div></div>
-                <div></div>
-              </div>
-
-              <div className="relative z-10">{children}</div>
-              
-              {/* footer */}
-            </main>
+                  <div className="relative z-10 min-h-[70vh]">{children}</div>
+                  
+                  {/* footer */}
+                  <footer className="border-t border-gray-800/50 py-8 px-6 max-w-7xl mx-auto">
+                    <div className="text-sm text-gray-400">Made with ❤️ by Satyam Singh </div>
+                  </footer>
+                </main>
+              </ConvexClientProvider>
+            </ClerkProvider>
           </ThemeProvider>
         
       </body>
---
+++ components/header.jsx
@@ -0,0 +1,99 @@
+"use client";
+
+import Link from 'next/link';
+import Image from 'next/image';
+import React, { useState } from 'react';
+import { SignInButton, UserButton } from '@clerk/nextjs';
+import { Button } from './ui/button';
+import { Authenticated, Unauthenticated } from 'convex/react';
+import { BarLoader } from 'react-spinners';
+import { useStoreUser } from '@/hooks/use-store-user';
+import { Building, Plus, Ticket } from 'lucide-react';
+
+const Header = () => {
+
+    const { isLoading } = useStoreUser();
+
+    const [showUpgradeModal, setShowUpgradeModal] = useState();
+
+
+  return (
+    <>
+        <nav className="fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-xl z-20 border-b">
+            <div className="max-w-7xl mx-auto px-6 py-4 flex item-center justify-between">
+                {/* Logo */}
+                <Link href={"/"} className="flex items-center">
+                    <Image 
+                        src="/spott.png" 
+                        alt="Spott logo"
+                        width={500}
+                        height={500}
+                        className="w-full h-11"
+                        priority
+                    />
+                </Link>
+
+                {/* Search & Location - Desktop Only */}
+
+                {/* Right Side Actions */}
+
+                <div className="flex items-center">
+                    <Button variant={"ghost"} size="sm" onClick={() => setShowUpgradeModal(true)}>
+                            Pricing
+                    </Button>                    <Button variant="ghost" size="sm" asChild className={"mr-2"}>
+                        <Link href="/explore">Explore</Link>
+                    </Button>
+                    <Authenticated>
+                        <Button size="sm" asChild className="flex gap-2 mr-4">
+                            <Link href="/create-event">
+                                <Plus className= "w-4 h-4"/>
+                                <span className="hidden sm:inline">Create Event</span>
+                            </Link>
+                        </Button>
+
+                        <UserButton>
+                            <UserButton.MenuItems>
+                                <UserButton.Link 
+                                    label="My Tickets"
+                                    labelIcon={<Ticket size={16}/>}
+                                    href="/my-ticket"
+                                />
+                                <UserButton.Link
+                                    label="My Events"
+                                    labelIcon={<Building size={16}/>}
+                                    href="/my-events"
+                                />
+
+                                <UserButton.Action label="manageAccount"/>
+                            </UserButton.MenuItems>
+                        </UserButton>
+                    </Authenticated>
+
+                    <Unauthenticated>
+                        <SignInButton mode="model">
+                            <Button size="sm">Sign In</Button>
+                        </SignInButton>
+                    </Unauthenticated>
+                    
+                </div>
+            </div>
+
+            {/* Mobile Search & Location - Below Header   */}
+
+            {/* Loader */}
+
+            {isLoading && (
+                <div className="absolute bottom-0 left-0 w-full">
+                    <BarLoader  width={"100%"} color="#a855f7"/>
+                </div>
+            )}
+
+
+        </nav>
+
+        {/* Models */}
+    </>
+  )
+}
+
+export default Header
